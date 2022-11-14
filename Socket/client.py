00dd6004a2e029b7ed73076214692a6c21d14490
Jules <jules.brutschy@gmail.com>
2022-11-14 11:46:05 +0100

    1 communication

--- Socket/client.py
+++ Socket/client.py
@@ -2,11 +2,31 @@ import socket
 
 host = '127.0.0.1'
 port = 10000
-message = 'hello'
-
+message = input(str('Entrez votre message: '))
 client_socket = socket.socket()
-client_socket.connect((host, port))
-client_socket.send(message.encode())
-data = client_socket.recv(1024).decode()
-print(data)
-client_socket.close()
+
+    
+if __name__ == '__main__':
+    client_socket.connect((host, port))
+    while message != 'bye' or message != 'arret':
+        message = input(str('Entrez votre message: '))
+        client_socket.send(message.encode())
+        data = client_socket.recv(1024).decode()
+        print(data)
+    else:
+        client_socket.close()
+
+
+
+
+    
+
+    """if __name__ == '__main__':
+    if message == 'bye' or message == 'arret':
+        client_socket.close()
+    else:
+        client_socket.connect((host, port))
+        client_socket.send(message.encode())
+        data = client_socket.recv(1024).decode()
+        print(data)"""
+    
--- Socket/serveur.py
+++ Socket/serveur.py
@@ -3,12 +3,32 @@ import socket
 host = '127.0.0.1'
 port = 10000
 reply = 'got it'
-
 server_socket = socket.socket()
-server_socket.bind((host, port))
-server_socket.listen(1)
-conn, address = server_socket.accept()
-data = conn.recv(1024).decode()
-print(data)
-conn.send(reply.encode())
-conn.close()
+
+
+if __name__ == '__main__':
+    server_socket.bind((host, port))
+    while reply != 'bye' or reply != 'arret':
+        server_socket.listen(1)
+        print("En attente d'un message...")
+        conn, address = server_socket.accept()
+        data = conn.recv(1024).decode()
+        print(data)
+        conn.send(reply.encode())
+
+    else:
+        server_socket.close()
+
+
+
+
+    """if reply == 'bye' or reply == 'arret':
+        server_socket.close()
+    else:
+        server_socket.bind((host, port))
+        server_socket.listen(1)
+        print("En attente d'un message...")
+        conn, address = server_socket.accept()
+        data = conn.recv(1024).decode()
+        print(data)
+        conn.send(reply.encode())"""
