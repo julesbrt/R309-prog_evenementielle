a69c2717f43b1ac834847e2f6634f84a12c52c22
Jules <jules.brutschy@gmail.com>
2022-11-15 10:31:55 +0100

    chat pt1 fonctionnel

--- Socket/client.py
+++ Socket/client.py
@@ -2,31 +2,23 @@ import socket
 
 host = '127.0.0.1'
 port = 10000
-message = input(str('Entrez votre message: '))
+message = ''
 client_socket = socket.socket()
+bye = 'bye'
+arret = 'arret'
+data = ''
 
-    
+
 if __name__ == '__main__':
     client_socket.connect((host, port))
-    while message != 'bye' or message != 'arret':
+    while message != bye and data != bye and message != arret and data != arret:
         message = input(str('Entrez votre message: '))
         client_socket.send(message.encode())
         data = client_socket.recv(1024).decode()
         print(data)
-    else:
-        client_socket.close()
-
-
-
-
     
+    print('Fermeture du client')
+    client_socket.close()
 
-    """if __name__ == '__main__':
-    if message == 'bye' or message == 'arret':
-        client_socket.close()
-    else:
-        client_socket.connect((host, port))
-        client_socket.send(message.encode())
-        data = client_socket.recv(1024).decode()
-        print(data)"""
-    
+
+    #cd .\Desktop\RT2\S3\R309-Prog_evenementielle\R309progeven\R309\Socket\
--- Socket/serveur.py
+++ Socket/serveur.py
@@ -3,32 +3,26 @@ import socket
 host = '127.0.0.1'
 port = 10000
 reply = 'got it'
+data = ''
+bye = 'bye'
+arret = 'arret'
 server_socket = socket.socket()
+server_socket.bind((host, port))
+server_socket.listen(1)
+
 
 
 if __name__ == '__main__':
-    server_socket.bind((host, port))
-    while reply != 'bye' or reply != 'arret':
-        server_socket.listen(1)
-        print("En attente d'un message...")
-        conn, address = server_socket.accept()
-        data = conn.recv(1024).decode()
-        print(data)
-        conn.send(reply.encode())
-
-    else:
-        server_socket.close()
-
-
+    while data != arret and reply != arret:
+        client_socket, address = server_socket.accept()
+        data = reply = ''
+        print('Connexion établie avec: ', str(address))
+        while data != bye and reply != bye and data != arret and reply != arret:
+            data = client_socket.recv(1024).decode()
+            print(data)
+            reply = input('Entrez votre réponse: ')
+            client_socket.send(reply.encode())
+        client_socket.close()
+    server_socket.close()
 
 
-    """if reply == 'bye' or reply == 'arret':
-        server_socket.close()
-    else:
-        server_socket.bind((host, port))
-        server_socket.listen(1)
-        print("En attente d'un message...")
-        conn, address = server_socket.accept()
-        data = conn.recv(1024).decode()
-        print(data)
-        conn.send(reply.encode())"""
